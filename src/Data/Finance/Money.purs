f5f27729af2a2e83a5a36fffd7e44c81b71974f0
rightfold <rightfold@gmail.com>
2017-03-24 14:48:44 +0100

    Add UnittedSemiring instance

--- src/Data/Finance/Money.purs
+++ src/Data/Finance/Money.purs
@@ -1,14 +1,11 @@
 module Data.Finance.Money
   ( Discrete(..)
-  , addDiscrete, ($+)
-  , subDiscrete, ($-)
-  , mulDiscrete, ($*)
-  , divDiscrete, ($/)
   , showDiscrete
   ) where
 
 import Data.Finance.Currency (kind Currency, class Currency, CProxy(..), decimals)
 import Data.Generic (class Generic)
+import Data.UnittedSemiring (class UnittedSemiring)
 import Prelude
 
 -- | An amount of money in the smallest discrete unit of a particular currency.
@@ -19,21 +16,11 @@ derive newtype instance eqDiscrete  :: Eq (Discrete c)
 derive newtype instance ordDiscrete :: Ord (Discrete c)
 derive instance genericDiscrete     :: Generic (Discrete c)
 
-addDiscrete :: ∀ c. Discrete c -> Discrete c -> Discrete c
-addDiscrete (Discrete a) (Discrete b) = Discrete (a + b)
-infixl 6 addDiscrete as $+
-
-subDiscrete :: ∀ c. Discrete c -> Discrete c -> Discrete c
-subDiscrete (Discrete a) (Discrete b) = Discrete (a - b)
-infixl 6 subDiscrete as $-
-
-mulDiscrete :: ∀ c. Discrete c -> Int -> Discrete c
-mulDiscrete (Discrete a) b = Discrete (a * b)
-infixl 7 mulDiscrete as $*
-
-divDiscrete :: ∀ c. Discrete c -> Int -> Discrete c
-divDiscrete (Discrete a) b = Discrete (a / b)
-infixl 7 divDiscrete as $/
+instance unittedSemiringDiscrete :: UnittedSemiring (Discrete c) Int where
+  uzero = Discrete 0
+  uone  = 1
+  uadd (Discrete a) (Discrete b) = Discrete (a + b)
+  umul (Discrete a) b            = Discrete (a * b)
 
 showDiscrete :: ∀ c. (Currency c) => Discrete c -> String
 showDiscrete (Discrete n) = showDiscrete' n (decimals (CProxy :: CProxy c))
